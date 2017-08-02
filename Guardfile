206bd11e2295897e96444f1fc3e3e665ff3b1150
neil patel <neil@myindianweddingapp.com>
2017-08-02 15:50:22 +0100

    Add user signin feature

--- Guardfile
+++ Guardfile
@@ -58,6 +58,8 @@ guard :rspec, cmd: "bin/rspec" do
     watch(%r{^app/models/(.+)\.rb$}) { |m| "spec/features/#{m[1]}s" }
     watch(%r{^app/controllers/(.+)_(controller)\.rb$}) { |m| "spec/features/#{m[1]}"}
     watch(rails.routes)          { "#{rspec.spec_dir}" }
+    watch(%r{^app/views/layouts/application.html.erb$}) { "spec/features" }
+
 
   watch(rails.spec_helper)     { rspec.spec_dir }
   watch(rails.routes)          { "#{rspec.spec_dir}/routing" }
---
+++ spec/features/users/signing_users_in_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.feature "Users signin" do
+  before do
+    @neil = User.create(email: "neil@example.com", password: "password")
+  end
+
+  scenario "with valid credentials" do
+    visit "/"
+    click_link "Sign in"
+    fill_in "Email", with: @neil.email
+    fill_in "Password", with: @neil.password
+    click_button "Log in"
+
+    expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_content("Signed in as #{@neil.email}")
+  end
+end
