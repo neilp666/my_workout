10359a2421648e8916f7bc4bdc4b2ad0cc7752ef
neil patel <neil@myindianweddingapp.com>
2017-08-03 11:36:47 +0100

    Create exercise unsuccessfully

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -4,14 +4,15 @@ RSpec.feature "Creating Exercise" do
   before do
     @neil = User.create(email: "neil@example.com", password: "password")
     login_as(@neil)
-  end
 
-  scenario "with valid inputs" do
     visit "/"
 
     click_link "My Workout"
     click_link "New Workout"
     expect(page).to have_link("Back")
+  end
+
+  scenario "with valid inputs" do
 
     fill_in "Duration", with: 70
     fill_in "Workout details", with: "Weight lifting"
@@ -26,20 +27,15 @@ RSpec.feature "Creating Exercise" do
   end
 
   scenario "with invalid inputs" do
-    visit "/"
-
-    click_link "My Workout"
-    click_link "New Workout"
-    expect(page).to have_link("Back")
-
+   
     fill_in "Duration", with: ""
-    fill_in "Workout details", with:""
-    fill_in "Activity date", with: ""
+    fill_in "Workout details", with: ""
+    fill_in "Activity date", with:  ""
     click_button "Create Exercise"
 
     expect(page).to have_content("Exercise has not been created")
-    expect(page).to have_content("Exercise has not been created")
-    expect(page).to have_content("Exercise has not been created")
-    expect(page).to have_content("Exercise has not been created")
+    expect(page).to have_content("Duration in min is not a number")
+    expect(page).to have_content("Workout details can't be blank")
+    expect(page).to have_content("Activity date can't be blank")
   end
 end
